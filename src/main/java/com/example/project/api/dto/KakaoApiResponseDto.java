0f68baef1def9318830dcd3c11f99396237f487e
Park Semin <semin6634@naver.com>
2025-03-26 11:29:23 +0900

    Kakao 주소 검색 api 구현

---
+++ src/main/java/com/example/project/api/dto/KakaoApiResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.project.api.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class KakaoApiResponseDto {
+    private MetaDto metaDto;
+    private List<DocumentDto> documentDtoList;
+}
